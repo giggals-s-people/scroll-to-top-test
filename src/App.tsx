3ac41aef6697d9f4d536f52e483a11f2d155fbdc
Amy-Eunji <104086211+Amy-Eunji@users.noreply.github.com>
2022-11-10 00:54:53 +0900

    GIGGAL-5 GIGGAL-6

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
 
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
