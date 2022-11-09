f1de92553eac6a891d3fe7b8d613d73b430cbfa0
Amy-Eunji <104086211+Amy-Eunji@users.noreply.github.com>
2022-11-10 00:52:11 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
 
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
