e817a1f6605bce264aa02e487a3b3fab8a903bc4
codeCYJ <67721584+codeCYJ@users.noreply.github.com>
2022-11-12 14:14:38 +0900

    scroll

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
 
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
